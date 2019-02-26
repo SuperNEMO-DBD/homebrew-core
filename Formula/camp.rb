4356254842a09fcf19236581b61007e7230a3176
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2019-02-26 11:36:51 +0000

    camp: New Formula

---
+++ Formula/camp.rb
@@ -0,0 +1,21 @@
+class Camp < Formula
+  desc "Tegesoft C++ reflection library"
+  homepage "https://github.com/tegesoft/camp"
+  url "https://github.com/drbenmorgan/camp.git", :revision => "7564e57f7b406d1021290cf2260334d57d8df255"
+  version "0.8.0"
+  revision 1
+
+  depends_on "cmake" => :build
+  depends_on "boost"
+
+  def install
+    ENV.cxx11
+    system "cmake", ".", *std_cmake_args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
