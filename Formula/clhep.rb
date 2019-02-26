05f04958092569e4340c6119cdd81390fabd3ebe
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2019-02-26 11:38:31 +0000

    clhep: Replace with SuperNEMO version

--- Formula/clhep.rb
+++ Formula/clhep.rb
@@ -1,40 +1,35 @@
 class Clhep < Formula
-  desc "Class Library for High Energy Physics"
-  homepage "https://proj-clhep.web.cern.ch/proj-clhep/"
-  url "https://proj-clhep.web.cern.ch/proj-clhep/DISTRIBUTION/tarFiles/clhep-2.4.1.0.tgz"
-  sha256 "d14736eb5c3d21f86ce831dc1afcf03d423825b35c84deb6f8fd16773528c54d"
+  desc "C++ Class Library for High Energy Physics"
+  homepage "http://proj-clhep.web.cern.ch/proj-clhep/"
+  revision 1
 
-  bottle do
-    cellar :any
-    sha256 "c067826cdae1e029b9e08a21e485484ad11947394060ffce21247d6f638dc49e" => :mojave
-    sha256 "ce6e421df5e06d3f85bc36c9eb4bb1e5ee5635d2a7b6a5c28d04490c12deb155" => :high_sierra
-    sha256 "3cfaf1bea915e52b57c44d7df7c40312fb7e0fab6cac5b8a8310ac4dcc55b74d" => :sierra
-    sha256 "4be2ee76db5d27890df9f0237e7c0342bcfae9f622dd3bfa156f6bbd79a2a2ee" => :el_capitan
-    sha256 "3a70cadd01ef398166e3328b659a68ec078a2b327a17a51420736dcfec91cc78" => :x86_64_linux
+  stable do
+    url "http://proj-clhep.web.cern.ch/proj-clhep/DISTRIBUTION/tarFiles/clhep-2.1.3.1.tgz"
+    sha256 "5d3e45b39a861731fe3a532bb1426353bf62b54c7b90ecf268827e50f925642b"
+
+    # Patch for clang compatibility, adapted from MacPorts
+    patch :DATA
   end
 
-  head do
-    url "https://gitlab.cern.ch/CLHEP/CLHEP.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
+  devel do
+    url "http://proj-clhep.web.cern.ch/proj-clhep/DISTRIBUTION/tarFiles/clhep-2.3.1.0.tgz"
+    sha256 "66272ae3100d3aec096b1298e1e24ec25b80e4dac28332b45ec3284023592963"
   end
 
   depends_on "cmake" => :build
 
   def install
-    mv (buildpath/"CLHEP").children, buildpath if build.stable?
-    mkdir "build" do
-      system "cmake", "..", *std_cmake_args
+    ENV.cxx11
+    mkdir "clhep-build" do
+      system "cmake", "../CLHEP", *std_cmake_args
       system "make", "install"
     end
   end
 
   test do
-    (testpath/"test.cpp").write <<~EOS
+    (testpath/"test.cpp").write <<-EOS.undent
       #include <iostream>
       #include <Vector/ThreeVector.h>
-
       int main() {
         CLHEP::Hep3Vector aVec(1, 2, 3);
         std::cout << "r: " << aVec.mag();
@@ -49,3 +44,22 @@ class Clhep < Formula
                  shell_output("./test").chomp
   end
 end
+
+__END__
+# https://savannah.cern.ch/bugs/?104110
+# https://trac.macports.org/ticket/42841
+#
+--- a/CLHEP/Matrix/src/Vector.cc.orig
++++ b/CLHEP/Matrix/src/Vector.cc
+@@ -114,9 +114,9 @@ HepVector::HepVector(const HepMatrix &hm1)
+ 
+ // trivial methods
+ 
+-inline int HepVector::num_row() const {return nrow;} 
+-inline int HepVector::num_size() const {return nrow;} 
+-inline int HepVector::num_col() const { return 1; }
++int HepVector::num_row() const {return nrow;}
++int HepVector::num_size() const {return nrow;}
++int HepVector::num_col() const { return 1; }
+ 
+ // operator()
