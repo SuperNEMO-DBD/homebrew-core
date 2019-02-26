dba2a3a00ceb596062e75b565585c40091f81a21
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2019-02-26 11:32:50 +0000

    xerces-c: Replace with SuperNEMO version

--- Formula/xerces-c.rb
+++ Formula/xerces-c.rb
@@ -1,48 +1,26 @@
 class XercesC < Formula
   desc "Validating XML parser"
   homepage "https://xerces.apache.org/xerces-c/"
-  url "https://www.apache.org/dyn/closer.cgi?path=xerces/c/3/sources/xerces-c-3.2.2.tar.gz"
-  sha256 "dd6191f8aa256d3b4686b64b0544eea2b450d98b4254996ffdfe630e0c610413"
+  url "https://archive.apache.org/dist/xerces/c/3/sources/xerces-c-3.1.4.tar.gz"
+  sha256 "c98eedac4cf8a73b09366ad349cb3ef30640e7a3089d360d40a3dde93f66ecf6"
+  revision 4
 
-  bottle do
-    cellar :any
-    rebuild 1
-    sha256 "fab62b22422c24b0218cae42f7f81ad736db316d9bde4218272cdf7b174c313f" => :mojave
-    sha256 "e62fba2c06fd03edf0491b54f753d10c4ca9e73e97c24389b749e655f9199b50" => :high_sierra
-    sha256 "8390cdf10fcc8b65a1f295eacf8b3fec34776d18219b8a8ce565592ee3b03372" => :sierra
-    sha256 "2171289b41e780789f1c93c784cb7243045ef836eb6ce36af8e84f219f0c1616" => :x86_64_linux
-  end
-
-  pour_bottle? do
-    reason "The bottle needs to be installed into #{Homebrew::DEFAULT_PREFIX}."
-    satisfy { OS.mac? || HOMEBREW_PREFIX.to_s == Homebrew::DEFAULT_PREFIX }
-  end
-
-  depends_on "cmake" => :build
   depends_on "curl" unless OS.mac?
+  depends_on "icu4c" unless OS.mac?
 
   def install
     ENV.cxx11
-
-    mkdir "build" do
-      system "cmake", "..", *std_cmake_args
-      system "make"
-      system "ctest", "-V"
-      system "make", "install"
-      system "make", "clean"
-      system "cmake", "..", "-DBUILD_SHARED_LIBS=OFF", *std_cmake_args
-      system "make"
-      lib.install Dir["src/*.a"]
-    end
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "install"
     # Remove a sample program that conflicts with libmemcached
     # on case-insensitive file systems
     (bin/"MemParse").unlink
   end
 
   test do
-    (testpath/"ducks.xml").write <<~EOS
+    (testpath/"ducks.xml").write <<-EOS.undent
       <?xml version="1.0" encoding="iso-8859-1"?>
-
       <ducks>
         <person id="Red.Duck" >
           <name><family>Duck</family> <given>One</given></name>
@@ -51,7 +29,6 @@ class XercesC < Formula
       </ducks>
     EOS
 
-    output = shell_output("#{bin}/SAXCount #{testpath}/ducks.xml")
-    assert_match "(6 elems, 1 attrs, 0 spaces, 37 chars)", output
+    assert_match /(6 elems, 1 attrs, 0 spaces, 37 chars)/, shell_output("#{bin}/SAXCount #{testpath}/ducks.xml")
   end
 end
