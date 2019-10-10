a9734b5e20ec54364a45897322883cd5c2a6aedc
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2019-10-10 15:44:53 +0100

    falaise: use non-redirecting link to archive

--- Formula/falaise.rb
+++ Formula/falaise.rb
@@ -1,7 +1,7 @@
 class Falaise < Formula
   desc "Simulation, Reconstruction and Analysis Software for SuperNEMO"
   homepage "https://github.com/supernemo-dbd/Falaise"
-  url "https://github.com/SuperNEMO-DBD/Falaise/archive/Falaise-4.0.1.tar.gz"
+  url "https://github.com/SuperNEMO-DBD/Falaise/archive/v4.0.1.tar.gz"
   sha256 "d350fd83e0dbf984a4032c3115d3c3a7266c528c3bcb33eb5ea263f6313a8e5c"
   head "https://github.com/SuperNEMO-DBD/Falaise.git", :branch => "develop"
 
