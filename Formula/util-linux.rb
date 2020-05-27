da8a0fb3f4082762a115c5848db5db952f685c52
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2020-05-27 18:06:36 +0100

    util-linux: Bump version to 2.32.1
    Supports newest GCC and glibc

--- Formula/util-linux.rb
+++ Formula/util-linux.rb
@@ -1,21 +1,13 @@
 class UtilLinux < Formula
   desc "Collection of Linux utilities"
   homepage "https://github.com/karelzak/util-linux"
-  url "https://www.kernel.org/pub/linux/utils/util-linux/v2.32/util-linux-2.32.1.tar.xz"
-  sha256 "86e6707a379c7ff5489c218cfaf1e3464b0b95acf7817db0bc5f179e356a67b2"
-  revision OS.mac? ? 1 : 2
+  url "https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v2.35/util-linux-2.35.2.tar.xz"
+  sha256 "21b7431e82f6bcd9441a01beeec3d57ed33ee948f8a5b41da577073c372eb58a"
+  revision 1 unless OS.mac?
 
-  bottle do
-    cellar :any
-    sha256 "977cf2845acc9cdcbcf1b7e92d9c0af0066c5d0cc17df307a123c13180a64e62" => :mojave
-    sha256 "d551dad77ab8c533bab98d5bd91291db1f296564336d59d600f0ce75496a9d08" => :high_sierra
-    sha256 "aeef9c88dd7ea82ac3f71b6f3793b2316b76ee59a8e01cc56f6316efa4e1346c" => :sierra
-    sha256 "f3040a39ad4ffb9eabd9446843dfc3b66df01b3264c875dc68e7339636830357" => :el_capitan
-    sha256 "02de29addaafdb6266cbf4d43c3baed3fc24a1c7067025837f52531c8836c12f" => :x86_64_linux
-  end
+  keg_only "macOS provides the uuid.h header" if OS.mac?
 
   unless OS.mac?
-    depends_on "supernemo-dbd/extra/linux-pam" => :optional
     depends_on "ncurses"
     depends_on "python"
   end
@@ -23,8 +15,6 @@ class UtilLinux < Formula
   conflicts_with "rename", :because => "both install `rename` binaries"
 
   def install
-    ENV.prepend_path "PATH", Formula["python"].opt_libexec/"bin" unless OS.mac?
-
     args = [
       "--disable-dependency-tracking",
       "--disable-silent-rules",
@@ -36,39 +26,39 @@ class UtilLinux < Formula
       args << "--disable-ipcrm" # does not build on macOS
       args << "--disable-wall" # already comes with macOS
       args << "--disable-libuuid" # conflicts with ossp-uuid
-      args << "--disable-libsmartcols" # macOS already ships 'column'
     else
       args << "--disable-use-tty-group" # Fix chgrp: changing group of 'wall': Operation not permitted
       args << "--disable-kill" # Conflicts with coreutils.
       args << "--disable-cal" # Conflicts with bsdmainutils
       args << "--without-systemd" # Do not install systemd files
       args << "--with-bashcompletiondir=#{bash_completion}"
+      args << "--disable-chfn-chsh"
+      args << "--disable-login"
+      args << "--disable-su"
+      args << "--disable-runuser"
+      args << "--disable-makeinstall-chown"
+      args << "--disable-makeinstall-setuid"
+      args << "--without-python"
+      args << "--without-readline"
     end
-
-    args += %w[
-      --disable-chfn-chsh
-      --disable-login
-      --disable-su
-      --disable-runuser
-      --disable-makeinstall-chown
-      --disable-makeinstall-setuid
-    ] if build.without? "linux-pam"
 
     system "./configure", *args
     system "make", "install"
 
-    # Remove binaries already shipped by macOS
-    %w[cal col colcrt colrm getopt hexdump logger nologin look mesg more renice rev ul whereis].each do |prog|
-      rm_f bin/prog
-      rm_f sbin/prog
-      rm_f man1/"#{prog}.1"
-      rm_f man8/"#{prog}.8"
-    end if OS.mac?
-
-    # these conflict with bash-completion-1.3
-    %w[chsh mount rfkill rtcwake].each do |prog|
-      rm_f bash_completion/prog
-    end unless OS.mac?
+    if OS.mac?
+      # Remove binaries already shipped by macOS
+      %w[cal col colcrt colrm getopt hexdump logger nologin look mesg more renice rev ul whereis].each do |prog|
+        rm_f bin/prog
+        rm_f sbin/prog
+        rm_f man1/"#{prog}.1"
+        rm_f man8/"#{prog}.8"
+      end
+    else
+      # these conflict with bash-completion-1.3
+      %w[chsh mount rfkill rtcwake].each do |prog|
+        rm_f bash_completion/prog
+      end
+    end
 
     # install completions only for installed programs
     Pathname.glob("bash-completion/*") do |prog|
@@ -81,8 +71,16 @@ class UtilLinux < Formula
   end
 
   test do
-    out = shell_output("#{bin}/namei -lx /usr").split("\n")
-    group = OS.mac? ? "wheel" : "root"
-    assert_equal ["f: /usr", "Drwxr-xr-x root #{group} /", "drwxr-xr-x root #{group} usr"], out
+    stat  = File.stat "/usr"
+    owner = Etc.getpwuid(stat.uid).name
+    group = Etc.getgrgid(stat.gid).name
+
+    flags = ["x", "w", "r"] * 3
+    perms = flags.each_with_index.reduce("") do |sum, (flag, index)|
+      sum.insert 0, ((stat.mode & (2 ** index)).zero? ? "-" : flag)
+    end
+
+    out = shell_output("#{bin}/namei -lx /usr").split("\n").last.split(" ")
+    assert_equal ["d#{perms}", owner, group, "usr"], out
   end
 end
